1b43cafedfbe45a1264ce1afe34bfff74dd3e495
shatminkhan0 <sharminakterkhan0@gmail.com>
2023-11-12 02:52:12 +0600

    add clear task functionality

---
+++ src/module/clearTask.js
@@ -0,0 +1,12 @@
+// cleare task functionality  
+
+import { tasks, storeTasksToLocalStorage } from './taskFunctions.js';
+
+const clearAllCompletedTasks = () => {
+  const filteredTasks = tasks.filter((task) => !task.completed);
+  tasks.length = 0;
+  tasks.push(...filteredTasks);
+  storeTasksToLocalStorage();
+};
+
+export default clearAllCompletedTasks;
