cb8449404853221aedfe5821a3cdfaf768b34dd2
shatminkhan0 <sharminakterkhan0@gmail.com>
2023-11-11 23:31:43 +0600

    Update task functionality

--- src/module/taskFunctions.js
+++ src/module/taskFunctions.js
@@ -1,16 +1,13 @@
 // taskfunctionality
 const tasks = JSON.parse(localStorage.getItem('Tasks')) || [];
-
 const storeTasksToLocalStorage = () => {
   localStorage.setItem('Tasks', JSON.stringify(tasks));
 };
-
 const sortTasks = () => {
   tasks.forEach((task, index) => {
     task.index = index + 1;
   });
 };
-
 const deleteTask = (index) => {
   tasks.splice(index, 1);
   tasks.forEach((task, newIndex) => {
@@ -19,22 +16,23 @@ const deleteTask = (index) => {
   sortTasks();
   storeTasksToLocalStorage();
 };
-
 const editTask = (description, index) => {
   tasks[index].description = description;
   storeTasksToLocalStorage();
 };
 
 const addTask = (taskDescription) => {
-  const newTask = {
-    description: taskDescription,
-    completed: false,
-    index: tasks.length + 1, // Set the index as array length + 1
-  };
-  tasks.push(newTask);
-  storeTasksToLocalStorage();
+  if (taskDescription.trim() !== '') {
+    const newTask = {
+      description: taskDescription,
+      completed: false,
+      index: tasks.length + 1,
+    };
+    tasks.push(newTask);
+    storeTasksToLocalStorage();
+  }
 };
 
 export {
-  tasks, storeTasksToLocalStorage, deleteTask, editTask, addTask,
+  tasks, storeTasksToLocalStorage, deleteTask, editTask, addTask, sortTasks,
 };
