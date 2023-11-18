92fd578b2fdb21340bfde5f1a00971e7deabac56
shatminkhan0 <sharminakterkhan0@gmail.com>
2023-11-18 15:26:30 +0600

    fix eslint error

--- src/module/taskFunctions.js
+++ src/module/taskFunctions.js
@@ -6,8 +6,6 @@ const storeTasksToLocalStorage = () => {
   localStorage.setItem('Tasks', JSON.stringify(tasks));
 };
 
-
-
 const sortTasks = () => {
   tasks.forEach((task, index) => {
     task.index = index + 1;
